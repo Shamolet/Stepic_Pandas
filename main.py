99bfe1ddbc8725e781083bf72cc4d234aa370b0d
Shamolet <crossfit2203@gmail.com>
2023-03-06 09:45:14 +0300

    ver 1

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 df = pd.read_csv('lesson_1_data.csv', encoding='Windows-1251', sep=';')  # parse_dates=['create_data', 'payment_data'])
 
-print(df.columns)
-
-print(df.shape)  # атрибут, хранящий данные о размере таблицы в виде кортежа
-
-print(df.dtypes)  # атрибут возвращает серию с описанием типа каждой колонки
-
-print(df.describe())  # метод для вывода описания числовых колонок в датафрейме
+# print(df.columns)
+# print(df.shape)  # атрибут, хранящий данные о размере таблицы в виде кортежа
+# print(df.dtypes)  # атрибут возвращает серию с описанием типа каждой колонки
+# print(df.describe())  # метод для вывода описания числовых колонок в датафрейме
 # (число строк, среднее значение, стандартное отклонение, мин, макс и
 # значения 25, 50, 75 квартилей)
+# print(df.head())  # посмотреть первые 5 строк
+# print(df.tail())  # посмотреть последние 5 строк
 
-print(df.head())  # посмотреть первые 5 строк
-print(df.tail())  # посмотреть последние 5 строк
+df = df.rename(columns={'Номер': 'number', 'Дата создания': 'date', 'Заработано': 'money'})
+print(df.dtypes)
