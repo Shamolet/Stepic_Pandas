67e119ab0c33b9a53837d7abffac087243f0b5d7
Shamolet <crossfit2203@gmail.com>
2023-03-06 12:19:38 +0300

    ver 2

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ df = pd.read_csv('lesson_1_data.csv', encoding='Windows-1251', sep=';')  # parse
 
 df = df.rename(columns={'Номер': 'number', 'Дата создания': 'date', 'Заработано': 'money'})
 print(df.dtypes)
+
+taxi = pd.read_csv('2_taxi_nyc.csv')
+# print(taxi.shape, taxi.head(), taxi.dtypes)
+taxi = taxi.rename(columns={'pcp 01': 'pcp_01', 'pcp 06': 'pcp_06', 'pcp 24':'pcp_24'})
+print(taxi.dtypes)
